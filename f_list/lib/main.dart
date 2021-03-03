f8201e2a1b706cf02d13fd3bb2157e5013e5f038
Josenaldo Júnior <josenaldo.asj@gmail.com>
2021-03-02 23:48:09 -0300

    Add form validation

--- f_list/lib/main.dart
+++ f_list/lib/main.dart
@@ -25,11 +25,17 @@ class TodoListScreen extends StatefulWidget {
 class _TodoListScreenState extends State<TodoListScreen> {
   final List<Task> _tasks = [];
   final TextEditingController _taskTitleController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  bool _autovalidate = false;
 
   @override
   void dispose() {
     _taskTitleController.dispose();
     super.dispose();
+  }
+
+  void _sortTasksUnfinishedFirst() {
+    _tasks.sort((task1, task2) => task1.finished ? 1 : -1);
   }
 
   @override
@@ -40,45 +46,73 @@ class _TodoListScreenState extends State<TodoListScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _taskTitleController,
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      autovalidateMode: _autovalidate
+                          ? AutovalidateMode.always
+                          : AutovalidateMode.disabled,
+                      decoration: InputDecoration(
+                          hintText: 'Nova tarefa', errorMaxLines: 2),
+                      controller: _taskTitleController,
+                      validator: (title) => title == null
+                          ? 'Nome da tarefa não pode ser vazio'
+                          : title.isEmpty
+                              ? 'Nome da tarefa não pode ser vazio'
+                              : null,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 4,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        if (formKey.currentState?.validate() ?? false) {
+                          setState(() {
+                            _autovalidate = false;
+                            _tasks.insert(
+                                0,
+                                Task(
+                                    finished: false,
+                                    title: _taskTitleController.text));
+                            _taskTitleController.text = '';
+                          });
+                        } else {
+                          setState(() {
+                            _autovalidate = true;
+                          });
+                        }
+                      },
+                      child: Text('Adicionar')),
+                ],
+              ),
+              SizedBox(height: 4),
+              Expanded(
+                child: RefreshIndicator(
+                  onRefresh: () async =>
+                      setState(() => _sortTasksUnfinishedFirst()),
+                  child: ListView.builder(
+                    itemBuilder: (context, index) => TodoListTile(
+                      onChanged: (finished) {
+                        setState(() {
+                          _tasks[index].finished = finished;
+                        });
+                      },
+                      task: _tasks[index],
+                    ),
+                    itemCount: _tasks.length,
                   ),
                 ),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _tasks.insert(
-                            0,
-                            Task(
-                                finished: false,
-                                title: _taskTitleController.text));
-                        _taskTitleController.text = '';
-                      });
-                    },
-                    child: Text('Adicionar')),
-              ],
-            ),
-            SizedBox(height: 4),
-            Expanded(
-              child: ListView.builder(
-                itemBuilder: (context, index) => TodoListTile(
-                  onChanged: (finished) {
-                    setState(() {
-                      _tasks[index].finished = finished;
-                    });
-                  },
-                  task: _tasks[index],
-                ),
-                itemCount: _tasks.length,
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
