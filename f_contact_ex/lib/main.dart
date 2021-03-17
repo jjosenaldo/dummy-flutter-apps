ef5acbadf9340043f43703799b2077d84ba2f42b
Josenaldo Júnior <josenaldo.asj@gmail.com>
2021-03-16 23:09:31 -0300

    Use GetIt for providing stuff

--- f_contact_ex/lib/main.dart
+++ f_contact_ex/lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:f_contact_ex/repository/impl/sqlite_contact_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 import 'app.dart';
 import 'repository/contact_repository.dart';
 import 'service/db_service.dart';
 
-void main() async {
+void _provideStuff() {
   final dbService = DBService();
-  final ContactRepository contactRepository =
-      SQLiteContactRepository(service: dbService);
-  await contactRepository.insert(
-    name: 'Josenaldi',
-    phone: '996152448',
-    email: 'jose',
-  );
-  (await contactRepository.findAll())
-      .forEach((contact) => debugPrint('$contact'));
+  final contactRepository = SQLiteContactRepository(service: dbService);
+  GetIt.I.registerSingleton<ContactRepository>(contactRepository);
+}
+
+void main() {
+  _provideStuff();
+
   runApp(App());
 }
